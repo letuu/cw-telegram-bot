a7bc69a86577592d5a59c1d557042b32c3ed6a0c
letuu <albertvt@ya.ru>
2023-01-22 13:21:33 +0300

    Initial commit

---
+++ src/main/java/pro/sky/telegrambot/CwTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CwTelegramBotApplication.class, args);
+    }
+
+}
